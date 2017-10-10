0ce9c6490d6ace5af630de3db949678cda09af36
Elf Sundae <elf.sundae@gmail.com>
2017-10-10 18:51:51 +0800

    test encoded result

--- tests/HelpersTest.php
+++ tests/HelpersTest.php
@@ -6,9 +6,10 @@ class HelpersTest extends TestCase
 {
     public function test_urlsafe_base64()
     {
-        for ($i = 0; $i < 10; $i++) {
-            $data = random_bytes(random_int(1, 100));
+        for ($i = 0; $i < 100; $i++) {
+            $data = random_bytes(random_int(1, 128));
             $encoded = urlsafe_base64_encode($data);
+            $this->assertSame(urlencode($encoded), $encoded);
             $decoded = urlsafe_base64_decode($encoded);
             $this->assertSame($data, $decoded);
         }
